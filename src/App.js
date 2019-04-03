5049b0655581acecc5414f1bad3c675a3cd5bf95
Nazmul Alom <nazmulshuvo03@gmail.com>
2019-04-04 04:05:44 +0600

    navbar started

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<table className="titleBar">
+					<tbody>
+						<tr>
+							<td>
+								<img width="50" src="movie_db.png" alt="icon" />
+							</td>
+							<td width="8" />
+							<td>
+								<h1>Movie Search</h1>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
 
 export default App;
